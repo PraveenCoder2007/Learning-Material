04a6e249fb43476ebafd367672d09f82b5502a93
praveen <praveen904265@gmail.com>
2025-01-24 21:09:39 +0530

    Initial Commit

---
+++ Learning Material/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learning Material
+//
+//  Created by praveen on 24/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learning Material/Learning_MaterialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_MaterialApp.swift
+//  Learning Material
+//
+//  Created by praveen on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Learning_MaterialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
